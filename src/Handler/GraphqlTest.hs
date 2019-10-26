b53ef7869580979af71063acc76320f451950718
Jorge Anez <jorge.anez@coxautoinc.com>
2019-10-25 21:53:57 -0400

    dammy graphql

--- src/Handler/GraphqlTest.hs
+++ src/Handler/GraphqlTest.hs
@@ -7,29 +7,54 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE QuasiQuotes           #-}
+
 
 module Handler.GraphqlTest (api) where
 
 
 import qualified Data.ByteString.Lazy.Char8 as B
 
+import           GHC.Generics
 import           Data.Morpheus              (interpreter)
 import           Data.Morpheus.Document     (importGQLDocumentWithNamespace)
-import           Data.Morpheus.Types        (GQLRootResolver (..), IORes)
+import           Data.Morpheus.Types        (queryResolver, Resolver (..), GQLRootResolver (..), IORes, GQLType (..))
+import           Data.Morpheus.Kind     (OBJECT)
 import           Data.Text                  (Text)
 import           Data.ByteString
+import           Handler.Deity  (Deity (..), dbDeity)
 
-importGQLDocumentWithNamespace "schema.gql"
 
-rootResolver :: GQLRootResolver IO () () (Query IORes) () ()
-rootResolver =
-  GQLRootResolver
-    {queryResolver = return Query {queryDeity}, mutationResolver = pure (), subscriptionResolver = pure ()}
-  where
-    queryDeity QueryDeityArgs {queryDeityArgsName} = pure Deity {deityName, deityPower}
-      where
-        deityName _ = pure "Morpheus"
-        deityPower _ = pure (Just "Shapeshifting")
+-- importGQLDocumentWithNamespace "schema.gql"
+
+data Query = Query
+  { deity :: DeityArgs -> IORes Deity
+  } deriving (Generic)
+
+instance GQLType Query where
+  type KIND Query = OBJECT
+  description _ = Just "Custom Description for Client Defined User Type"
+
+data DeityArgs = DeityArgs
+  { name      :: Text  -- Required Argument
+  , mythology :: Maybe Text  -- Optional Argument
+  } deriving (Generic)
+
+
+-- resolveDeity :: DeityArgs -> IORes Deity
+-- resolveDeity args = queryResolver $ dbDeity  (name args) (mythology args)
+
+rootResolver :: GQLRootResolver IO () () Query () ()
+rootResolver = GQLRootResolver { queryResolver = return Query {deity}
+                               , mutationResolver = return ()
+                               , subscriptionResolver = return ()
+                               }
+            where
+                deity DeityArgs{name, mythology} = pure Deity {fullName, power}
+                  where
+                          fullName = "Hi" <> name
+                          power = Just "Shapeshifting"
 
 api :: ByteString -> IO ByteString
 api = interpreter rootResolver
--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -9,6 +9,13 @@ module Handler.Home where
 import Import
 import Text.Hamlet          (hamletFile)
 import Handler.GraphqlTest (api)
+import qualified Data.Text as T
+import qualified Data.ByteString.Char8 as B
+import Network.Wai.Conduit (sourceRequestBody)
+import Data.Conduit.Lazy (lazyConsume)
+import qualified Data.Conduit.Text as CT
+import qualified Data.Conduit.List as CL
+-- import qualified Data.ByteString.Lazy as LB
 
 -- getHomeR :: Handler Html
 -- getHomeR = homePage
@@ -23,7 +30,18 @@ getHomeR :: Handler TypedContent
 getHomeR = do
           result <- liftIO $ api "query GetDeity { deity (name: \"Morpheus\") { fullName power } }"
           return $ TypedContent jsonType $ toContent $ result
- 
+
+postHomeR :: Handler TypedContent
+postHomeR = do
+             r <- rawRequestBody $$ CT.decode CT.utf8 =$ CL.consume
+             let [x] = r
+             result <- liftIO $ api $ B.pack $ T.unpack x
+--              result <- liftIO $ api $ B.pack $ T.unpack "query{ deity { fullName } }"
+--              () <- do
+--                     $logDebug $ T.pack $ B.unpack request
+--                     return ()
+             return $ TypedContent jsonType $ toContent result
+
 {--
         case maid of
              Nothing -> redirect ("auth/page/github/forward" :: Text)
