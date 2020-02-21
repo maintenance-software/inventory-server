270e3a98d2b139db21dc698d01f14a98553b8ec3
Jorge Anez <jorge.aniez@outlook.com>
2020-02-21 01:25:57 -0400

    add build ui

--- Setup.hs
+++ Setup.hs
@@ -7,10 +7,23 @@ import qualified System.Process as Proc
 import qualified System.Directory as Dir
 import Control.Monad (when, void)
 
+buildUI :: IO ()
+buildUI = do
+            distExists <- Dir.doesDirectoryExist "webapps/dist"
+            () <- case distExists of
+                    True -> Dir.removeDirectoryRecursive "webapps/dist"
+                    False -> return ()
+            () <- Dir.createDirectory "webapps/dist"
+            Proc.system "cd ui-home && yarn install && yarn build"
+            Proc.system "cp -a ui-home/build/. webapps/dist"
+            return ()
+
 buildHook_ :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 buildHook_ packageDesc localBuildInfo userHooks buildFlags = do
-    exists <- Dir.doesFileExist "./app/dist/app.bundle.js"
-    when (not exists) $ void $ Proc.system "cd app && npm install && npm run build-js"
+--    exists <- Dir.doesFileExist "./app/dist/app.bundle.js"
+--    when (not exists) $ void $ Proc.system "cd app && npm install && npm run build-js"
+    () <- buildUI
+--    print buildFlags
     (buildHook DS.simpleUserHooks) packageDesc localBuildInfo userHooks buildFlags
 
 main :: IO ()
